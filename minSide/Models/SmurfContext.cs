bc6b57061bf72bb8ee6a607bc38948eda3f86f45
tocvfan <christian@hammervig.dk>
2017-09-28 22:49:01 +0200

    First commit

---
+++ minSide/Models/SmurfContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace minSide.Models {
+    public class SmurfContext : DbContext, ISmurfContext {
+        public DbSet<Photo> Photos { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+        IQueryable<Photo> ISmurfContext.Photos {
+            get { return Photos; }
+        }
+        IQueryable<Comment> ISmurfContext.Comments {
+            get { return Comments; }
+        }
+        public T Add<T>(T entity) where T : class {
+            throw new NotImplementedException();
+        }
+
+        public T Delete<T>(T entity) where T : class {
+            throw new NotImplementedException();
+        }
+
+        public Comment FindCommentById(int ID) {
+            throw new NotImplementedException();
+        }
+
+        public Photo FindPhotoById(int ID) {
+            throw new NotImplementedException();
+        }
+
+        public Photo FindPhotoByTitle(string Title) {
+            throw new NotImplementedException();
+        }
+    }
+}
